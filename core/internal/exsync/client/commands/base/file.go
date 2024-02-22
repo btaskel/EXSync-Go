f1fe6acadd5985d7a14af762f20537f11e2be75a
btaskel <bt_asker@hotmail.com>
2024-02-22 11:43:24 +0800

    Client端文件拉取增加写入缓冲区、远程报错处理

--- core/internal/exsync/client/commands/base/file.go
+++ core/internal/exsync/client/commands/base/file.go
@@ -2,6 +2,7 @@ package base
 
 import (
 	"EXSync/core/internal/config"
+	"EXSync/core/internal/modules/buffer"
 	"EXSync/core/internal/modules/hashext"
 	"EXSync/core/internal/modules/pathext"
 	"EXSync/core/internal/modules/socket"
@@ -10,16 +11,33 @@ import (
 	"EXSync/core/option/exsync/comm"
 	"errors"
 	"github.com/sirupsen/logrus"
+	"net"
 	"os"
 	"path"
 	"sync"
 )
 
+var (
+	FileQueryErr  = errors.New("FileQueryErr")  // 查询数据不存在
+	FileSyncDBErr = errors.New("FileSyncDBErr") // 文件与数据库不同步
+	FileOpenErr   = errors.New("FileOpenErr")   // 文件打开失败
+	FileCloseErr  = errors.New("FileCloseErr")  // 文件关闭失败
+
+	SocketNewSessionErr = errors.New("SocketNewSessionErr") // 创建Session对象失败
+	SocketTimeoutErr    = errors.New("SocketTimeoutErr")    // 接收数据超时
+	SocketBufferErr     = errors.New("SocketBufferErr")     // 文件写入缓冲遇到错误
+
+	ParamsNotExistsErr = errors.New("ParamsNotExistsErr") // 缺少必要的参数
+)
+
+type FileErr struct {
+}
+
 // GetFile 根据相对路径列表在Space里搜索相应的文件，并获取到本机。
 // relPaths的数量将决定以多少并发获取文件
 // 如果本地文件是待续写文件，将会续写
 // 如果本地文件不存在，将会创建
-func (b *Base) GetFile(relPaths, outPaths []string, space configOption.UdDict) (failedFiles []string, err error) {
+func (b *Base) GetFile(relPaths, outPaths []string, space configOption.UdDict) (failedFiles map[string]error, err error) {
 	// 初始化GetFile
 	var (
 		hashList      []string
@@ -34,6 +52,7 @@ func (b *Base) GetFile(relPaths, outPaths []string, space configOption.UdDict) (
 		panic("GetFile:获取的文件路径与保存路径数量不同步！")
 	}
 
+	// 准备文件信息
 	for _, relPath := range relPaths {
 		fileMark := hashext.GetRandomStr(8)
 		replyMark := hashext.GetRandomStr(8)
@@ -41,14 +60,14 @@ func (b *Base) GetFile(relPaths, outPaths []string, space configOption.UdDict) (
 		file, err := sqlt.QueryFile(space.Db, relPath)
 		if err != nil {
 			logrus.Errorf("Active GetFile: Error querying information for file %s!", relPath)
-			failedFiles = append(failedFiles, relPath)
+			failedFiles[relPath] = FileQueryErr
 		}
 		fileStat, err := os.Stat(path.Join(space.Path, relPath))
 		if err == nil {
 			fs := fileStat.Size()
 			if fs != 0 && fs != file.Size {
 				// 索引与本地文件实际情况不同步
-				return
+				failedFiles[relPath] = FileSyncDBErr
 			}
 		}
 
@@ -58,7 +77,12 @@ func (b *Base) GetFile(relPaths, outPaths []string, space configOption.UdDict) (
 		fileMarkList = append(fileMarkList, fileMark)
 	}
 
-	// 准备数据
+	// 如果准备文件信息有错误, 则返回错误map
+	if len(failedFiles) != 0 {
+		return
+	}
+
+	// 发送数据
 	command := comm.Command{
 		Command: "data",
 		Type:    "file",
@@ -74,21 +98,20 @@ func (b *Base) GetFile(relPaths, outPaths []string, space configOption.UdDict) (
 	}
 	session, err := socket.NewSession(b.TimeChannel, nil, b.CommandSocket, hashext.GetRandomStr(8), b.AesGCM)
 	if err != nil {
-		return relPaths, err
+		return nil, err
 	}
 	defer session.Close()
 	_, err = session.SendCommand(command, false, true)
 	if err != nil {
-		return relPaths, err
+		return nil, err
 	}
 
 	// 初始化传输
 	dataBlock := int64(config.PacketSize - 8 - b.EncryptionLoss)
 
 	// 单文件传输线程
-	subRoutine := func(i int, wait *sync.WaitGroup) {
+	subRoutine := func(i int, wait *sync.WaitGroup, failFilesChan chan map[string]error) {
 		defer wait.Done()
-
 		localFileHash := hashList[i]
 		localFileSize := sizeList[i]
 		fileMark := fileMarkList[i]
@@ -108,125 +131,162 @@ func (b *Base) GetFile(relPaths, outPaths []string, space configOption.UdDict) (
 		s, err := socket.NewSession(b.TimeChannel, b.DataSocket, nil, replyMark, b.AesGCM)
 		defer s.Close()
 		if err != nil {
+			failFilesChan <- map[string]error{
+				outPath: SocketNewSessionErr,
+			}
 			return
 		}
-		reply, ok := s.Recv()
-		if !ok {
+
+		reply, err := s.Recv()
+		if err != nil {
+			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				failFilesChan <- map[string]error{
+					outPath: SocketNewSessionErr,
+				}
+				return
+			}
+			failFilesChan <- map[string]error{
+				outPath: err,
+			}
 			return
 		}
 
 		// 处理远程文件状态
 		remoteFileSize, ok := reply.Data["size"].(int64)
 		if !ok {
+			socket.SendStat(s, "Active-GetFile-subRoutine: Missing parameter <size>!")
 			return
 		}
 		remoteFileHash, ok := reply.Data["hash"].(string)
-
-		if remoteFileSize == 0 {
-			logrus.Errorf("active Client GetFile: File %s failed to retrieve from host %s", filePath, b.Ip)
+		if !ok {
+			socket.SendStat(s, "Active-GetFile-subRoutine: Missing parameter <hash>!")
 			return
 		}
 
-		if remoteFileHash != localFileHash {
-			if remoteFileSize > localFileSize && localFileSize != 0 {
-				// 1.远程文件大于本地文件，等待对方判断是否为需续写文件；
-				// 2.本地文件不存在，需要对方发送文件；
+		if remoteFileSize == 0 {
+			logrus.Errorf("Active-GetFile-subRoutine: File %s failed to retrieve from host %s", filePath, b.Ip)
+			return
+		}
+		// 初始化文件写入缓冲区
+		fileBuf := buffer.File{
+			TimeChannel: b.TimeChannel,
+			FileMark:    fileMark,
+			DataBlock:   dataBlock,
+		}
+		if remoteFileHash == localFileHash {
+			// 正在尝试获取一个同样的文件，没有意义。
+		}
+		if remoteFileSize > localFileSize && localFileSize != 0 {
+			// 1.远程文件大于本地文件，等待对方判断是否为需续写文件；
+			// 2.本地文件不存在，需要对方发送文件；
 
-				reply, ok = s.RecvTimeout(int(remoteFileSize / 1048576))
-				if !ok {
-					return
-				}
-				pass, ok := reply.Data["ok"].(bool)
-				if !ok || !pass {
-					return
-				}
-
-				pathext.MakeDir(filePath)
-				f, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0667)
-				if err != nil {
-					return
-				}
-				defer f.Close()
-				for {
-					result, err := b.TimeChannel.GetTimeout(fileMark, config.SocketTimeout)
-					if err != nil {
-						if err == errors.New("timeout") {
-							logrus.Errorf("active Sync Space %s :Receiving %s file timeout!", space.SpaceName, filePath)
-						} else {
-							logrus.Errorf("active Sync Space %s :Unknown error receiving %s file from host %s", space.SpaceName, filePath, b.Ip)
-						}
-						return
-					}
-					_, err = f.Write(result)
-					if err != nil {
-						return
-					}
-					localFileSize += dataBlock
-					if localFileSize >= remoteFileSize {
-						break
-					}
+			reply, ok = s.RecvTimeout(int(remoteFileSize/1048576) + 3)
+			if !ok {
+				logrus.Errorf("Active-GetFile-subRoutine: Received file renewal %s reply timeout!", outPath)
+				failFilesChan <- map[string]error{
+					outPath: SocketTimeoutErr,
 				}
 				return
-
-			} else if remoteFileSize > localFileSize && localFileSize == 0 {
-				// 本地文件不存在
-				pathext.MakeDir(filePath)
-				f, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0667)
-				if err != nil {
-					return
-				}
-				defer f.Close()
-				// 启用缓存
-				for {
-					result, err := b.TimeChannel.GetTimeout(fileMark, config.SocketTimeout)
-					if err != nil {
-						if err == errors.New("timeout") {
-							logrus.Errorf("active Sync Space %s :Receiving %s file timeout!", space.SpaceName, filePath)
-						} else {
-							logrus.Errorf("active Sync Space %s :Unknown error receiving %s file from host %s", space.SpaceName, filePath, b.Ip)
-						}
-						return
-					}
-					_, err = f.Write(result)
-					if err != nil {
-						return
-					}
-					localFileSize += dataBlock
-					if localFileSize >= remoteFileSize {
-						break
-					}
-				}
-
-			} else if remoteFileSize == localFileSize {
-				// 1.文件大小相同，哈希值不同；
-				// 2.本地或远程文件不存在；
-				if remoteFileSize == 0 {
-					// 远程文件不存在
-					return
-				} else {
-					// 文件不同
-					return
-				}
-
-			} else if remoteFileSize < localFileSize {
-				// 1.远程文件小于本地文件，不进行同步
-				// 2.在获取一个远程不存在的文件
 			}
-		} else {
-			// 正在尝试获取一个同样的文件，没有意义。
+			pass, ok := reply.Data["ok"].(bool)
+			if !ok || !pass {
+				failFilesChan <- map[string]error{
+					outPath: ParamsNotExistsErr,
+				}
+				return
+			}
+
+			pathext.MakeDir(filePath)
+			f, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0667)
+			if err != nil {
+				failFilesChan <- map[string]error{
+					outPath: FileOpenErr,
+				}
+				return
+			}
+			defer func(f *os.File) {
+				err = f.Close()
+				if err != nil {
+					logrus.Error(err)
+					failFilesChan <- map[string]error{
+						outPath: FileCloseErr,
+					}
+					return
+				}
+			}(f)
+			// 创建文件缓冲区(1MB)
+			err = fileBuf.FileWrite(f, localFileSize, remoteFileSize)
+			if err == errors.New("timeout") {
+				logrus.Errorf("Active-GetFile-subRoutine: Sync Space %s :Receiving %s file timeout!", space.SpaceName, filePath)
+			} else {
+				logrus.Errorf("Active-GetFile-subRoutine: Sync Space %s :Unknown error receiving %s file from host %s! %s", space.SpaceName, filePath, b.Ip, err)
+			}
+			return
+
+		} else if remoteFileSize > localFileSize && localFileSize == 0 {
+			// 本地文件不存在
+			pathext.MakeDir(filePath)
+			f, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0667)
+			if err != nil {
+				failFilesChan <- map[string]error{
+					outPath: FileOpenErr,
+				}
+				return
+			}
+			defer func(f *os.File) {
+				err := f.Close()
+				if err != nil {
+					logrus.Error(err)
+					failFilesChan <- map[string]error{
+						outPath: FileCloseErr,
+					}
+					return
+				}
+			}(f)
+
+			// 创建文件缓冲区(1MB)
+			err = fileBuf.FileWrite(f, localFileSize, remoteFileSize)
+			if err == errors.New("timeout") {
+				logrus.Errorf("Active-GetFile-subRoutine: Sync Space %s :Receiving %s file timeout!", space.SpaceName, filePath)
+				failFilesChan <- map[string]error{
+					outPath: SocketTimeoutErr,
+				}
+				return
+			} else {
+				logrus.Errorf("Active-GetFile-subRoutine: Sync Space %s :Unknown error receiving %s file from host %s! %s", space.SpaceName, filePath, b.Ip, err)
+				failFilesChan <- map[string]error{
+					outPath: SocketBufferErr,
+				}
+			}
+
+		} else if remoteFileSize == localFileSize {
+			// 1.文件大小相同，哈希值不同；
+			// 2.本地或远程文件不存在；
+			if remoteFileSize == 0 {
+				// 远程文件不存在
+				return
+			} else {
+				// 文件不同
+				return
+			}
+
+		} else if remoteFileSize < localFileSize {
+			// 1.远程文件小于本地文件，不进行同步
+			// 2.在获取一个远程不存在的文件
 		}
 	}
 
 	// 开始传输
-	logrus.Debugf("active Sync Space %s :File %s begins to transfer", space.SpaceName, relPaths)
+	logrus.Debugf("Active-GetFile: Sync Space %s :File %s begins to transfer", space.SpaceName, relPaths)
 	var wait sync.WaitGroup
+	failFileChannel := make(chan map[string]error, len(relPaths))
 	fileCount := len(relPaths)
 	wait.Add(fileCount)
 	for i := 0; i < fileCount; i++ {
-		go subRoutine(i, &wait)
+		go subRoutine(i, &wait, failFileChannel)
 	}
 	wait.Wait()
-	logrus.Debugf("active Sync Space %s :File %s transfer completed", space.SpaceName, relPaths)
+	logrus.Debugf("Active-GetFile: Sync Space %s :File %s transfer completed", space.SpaceName, relPaths)
 	return
 }
 
