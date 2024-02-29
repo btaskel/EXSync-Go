1ae0e574c33ac9cddb9518224918b352be61da6f
Sha110w_4qua <49711457+shallow-zzt@users.noreply.github.com>
2024-02-29 17:07:49 +0800

    添加api查询url

--- webui/main.go
+++ webui/main.go
@@ -8,6 +8,10 @@ import (
 func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("static")))
+	http.HandleFunc("/api/config", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+	
 	//webui端口
 	http.ListenAndServe(":8080", nil)
 	fmt.Println("running……")
